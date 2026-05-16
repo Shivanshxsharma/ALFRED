f885a9269772d4fe90984373516bed1ec676b227
Shivanshxsharma <shivanshsharma9858@gmail.com>
2026-05-16 16:18:48 +0530

    refactor: update Loader component styles and animation

--- alfred/src/components/ui/Loader.jsx
+++ alfred/src/components/ui/Loader.jsx
@@ -1,48 +1,35 @@
 import React from 'react';
 export default function Loader() {
   return (
-    <div className="flex items-center justify-start">
-      <div className="loader"></div>
-      
-      <style jsx>{`
+    <>
+      <style>{`
         .loader {
-          width: 40px;
-          aspect-ratio: 1;
-          --c: linear-gradient(#fff 0 0);
-          --m: radial-gradient(farthest-side, #fff 92%, #0000);
-          background: 
-            var(--m) center / 12px 12px,
-            var(--c) left 50% top -20px / 8px 16px, 
-            var(--c) left 50% bottom -20px / 8px 16px, 
-            var(--c) top 50% left -20px / 16px 8px, 
-            var(--c) top 50% right -20px / 16px 8px;
-          background-repeat: no-repeat;
-          animation: 
-            loader-1 1s infinite,
-            loader-2 1s infinite;
+          width: 70px;
+          height: 26px;
+          background: #475569;
+          border-radius: 50px;
+          --c: no-repeat radial-gradient(farthest-side, #fff 92%, #0000);
+          --s: 18px 18px;
+          -webkit-mask:
+            var(--c) left 4px top 50%,
+            var(--c) center,
+            var(--c) right 4px top 50%,
+            linear-gradient(#000 0 0);
+          -webkit-mask-composite: xor;
+          mask-composite: exclude;
+          animation: l1 1.5s infinite;
         }
-        
-        @keyframes loader-1 {
-          30%,
-          70% {
-            background-position: 
-              center,
-              left 50% top calc(50% - 8px),
-              left 50% bottom calc(50% - 8px),
-              top 50% left calc(50% - 8px),
-              top 50% right calc(50% - 8px);
-          }
-        }
-        
-        @keyframes loader-2 {
-          0%, 40% {
-            transform: rotate(0);
-          }
-          60%, 100% {
-            transform: rotate(90deg);
-          }
+        @keyframes l1 {
+          0%     { -webkit-mask-size: 0 0,      0 0,      0 0,      auto }
+          16.67% { -webkit-mask-size: 18px 18px, 0 0,      0 0,      auto }
+          33.33% { -webkit-mask-size: 18px 18px, 18px 18px, 0 0,      auto }
+          50%    { -webkit-mask-size: 18px 18px, 18px 18px, 18px 18px, auto }
+          66.67% { -webkit-mask-size: 0 0,      18px 18px, 18px 18px, auto }
+          83.33% { -webkit-mask-size: 0 0,      0 0,       18px 18px, auto }
+          100%   { -webkit-mask-size: 0 0,      0 0,       0 0,       auto }
         }
       `}</style>
-    </div>
-  );
+      <div className="loader" />
+    </>
+  )
 }
